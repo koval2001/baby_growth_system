84b8e6cd15d4bdf0bd0cb4fd6781a7ec75df89e4
koval2001 <d.koval@geniusee.com>
2022-10-16 19:46:15 +0300

    cleaned code

--- functions/index.js
+++ functions/index.js
@@ -4,13 +4,8 @@ admin.initializeApp();
 
 exports.myFunctionName = functions.firestore
   .document('measures/{docId}')
-  .onCreate((snap, context) => {
-    // Get an object representing the document
-    // e.g. {'name': 'Marie', 'age': 66}
+  .onCreate((snap) => {
     const newValue = snap.data();
-
-    // access a particular field as you would any JS property
     const { title } = newValue;
-
     console.log('New document with title:' + title + ' was created');
   });
